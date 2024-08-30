eda794af04751a00d12a7cf0b990f6532ed4d1e9
anaghasimha <anagha.mysore2000@gmail.com>
2024-08-30 17:47:31 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Anagha Simha',
+    author_email= 'anagha.mysore2000@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
