caf9ed76a2541bdbe84a0b954b589bbe07c7a293
anaghasimha <anagha.mysore2000@gmail.com>
2024-09-01 15:08:38 -0500

    logging exception and utils added

--- template.py
+++ template.py
@@ -2,46 +2,51 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
 
 project_name = "wasteDetection"
 
 list_of_files = [
     ".github/workflows/.gitkeep",
-    "data..gitkeep",
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/components/data_ingestion.py",
-    f"src/{project_name}/components/data_validation.py",
-    f"src/{project_name}/components/model_trainer.py",
-    f"src/{project_name}/constants/__init__.py",
-    f"src/{project_name}/constants/training_pipeline/__init__.py",
-    f"src/{project_name}/constants/application.py",
-    f"src/{project_name}/entity/artifacts_entity.py",
-    f"src/{project_name}/exception/__init__.py",
-    f"src/{project_name}/logger/__init__.py",
-    f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/pipeline/training_pipeline.py",
-    f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/utils/main_utils.py",
-    "research/trials.ipynb",
+    "data/.gitkeep",
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/constant/__init__.py",
+    f"{project_name}/constant/training_pipeline/__init__.py",
+    f"{project_name}/constant/application.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/entity/artifacts_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "reseach/trials.ipynb",
     "templates/index.html",
     "app.py",
     "Dockerfile",
     "requirements.txt",
-    "setup.py"
+    "setup.py",
 ]
 
-for file_path in list_of_files:
-    file_path = Path(file_path)
-    
-    filedir,filename = os.path.split(file_path)
-    if filedir != "":
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Created directory: {filedir} for the file: {filename}")
 
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir!="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
+
+    
     if(not os.path.exists(filename)) or (os.path.getsize(filename) == 0):
-        with open(file_path, "w") as f:
+        with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filename}")
 
