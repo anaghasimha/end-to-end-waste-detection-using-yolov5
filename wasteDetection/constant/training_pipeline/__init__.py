a2113f1a066a213e79088d44803c13e63171404a
anaghasimha <anagha.mysore2000@gmail.com>
2024-09-03 14:55:28 -0500

    data validation

--- wasteDetection/constant/training_pipeline/__init__.py
+++ wasteDetection/constant/training_pipeline/__init__.py
@@ -13,7 +13,7 @@ DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/1ECfl3dtYyfivY8kYPq7RH
 
 
 """
-Data Validation realted contant start with DATA_VALIDATION VAR NAME
+Data Validation realted constant start with DATA_VALIDATION VAR NAME
 """
 
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
